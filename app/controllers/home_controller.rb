e87bc3151b36d0bc34fdf2798143f98690f27abf
SwapnilPatil <swapnil.patil04@gmail.com>
2014-11-08 17:38:15 +0530

    worked on UI

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,7 +9,7 @@ class HomeController < ApplicationController
 
 
   def dashboard
-    @job_activities = JobActivity.where(user_id: 4, status: ['PENDING', 'WIP']).order("id desc").paginate(:page => params[:page])
+    @job_activities = JobActivity.where(user_id: current_user.id, status: ['PENDING', 'WIP']).order("id desc").paginate(:page => params[:page])
     render layout: 'application1'
     
   end
